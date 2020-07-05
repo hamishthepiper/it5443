c32a9f432b502479ae0365a17101f405e55071a1
hamishthepiper <james.stephens88@gmail.com>
2020-07-05 11:31:28 -0400

    Comments

--- setheader.js
+++ setheader.js
@@ -1,8 +1,9 @@
-//Sets the current active window dynamically
+//Sets the current active window dynamically using RegEx to grab the page name
 var re = /\/(\w+)\.php$/;
 let page = re.exec(window.location.pathname)
 
 if (!page) { 
+    // The RegEx expression won't grab the root "/" reliably, so this is the default setting
     document.getElementById("index").classList.add("active");
 } else {
     document.getElementById(page[1]).classList.add("active");
